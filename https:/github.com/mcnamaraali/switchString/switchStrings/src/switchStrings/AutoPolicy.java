6ceedb4f515039d35db2014dfdfb620ea76f376d
alimcnamara <mcnamaraali@gmail.com>
2020-03-12 17:01:46 +0100

    additional comments

--- https:/github.com/mcnamaraali/switchString/switchStrings/src/switchStrings/AutoPolicy.java
+++ https:/github.com/mcnamaraali/switchString/switchStrings/src/switchStrings/AutoPolicy.java
@@ -12,40 +12,48 @@ public class AutoPolicy {
 		this.state = state;
 	}
 	
+	//sets the accountNumber 
 	public void setAccountNumber (int accountNumber) {
 		this.accountNumber = accountNumber;
 	}
 	
+	//gets the accountNumber
 	public int getAccountNumber () {
 		return accountNumber;
 	}
 	
+	//sets the Make and Model of the car
 	public void setMakeAndModel (String makeAndModel) {
 		this.makeAndModel = makeAndModel;
 	}
 	
+	//returns the make and model
 	public String getMakeAndModel () {
 		return makeAndModel;
 	}
 	
+	//sets the state
 	public void setState (String state) {
 		this.state = state;
 	}
 	
+	//returns the state
 	public String getState () {
 		return state;
 	}
 	
+	//predicate method returns the state has no-fault insurance
 	public boolean isNoFaultState() {
 		boolean noFaultState;
 		
+		//determine whether state has no-fault car insurance
 		switch (getState()) {
-		case "LK" : case "GY" : case "KY":
-			noFaultState = true;
-			break;
-		default:
-			noFaultState = false;
-			break;
+			case "LK" : case "GY" : case "KY":
+				noFaultState = true;
+				break;
+			default:
+				noFaultState = false;
+				break;
 		}
 		return noFaultState;
 		
